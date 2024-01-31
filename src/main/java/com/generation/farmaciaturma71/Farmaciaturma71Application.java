d77d648ea21106eff5f6d8e33be99a6ab5ee7418
NataliaOlgado <151691956+NataliaOlgado@users.noreply.github.com>
2024-01-31 12:07:29 -0300

    Projeto Completo
    Projeto realizado ate onde foi solicitado, security, Junits testados e documentação.

---
+++ src/main/java/com/generation/farmaciaturma71/Farmaciaturma71Application.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaturma71;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farmaciaturma71Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farmaciaturma71Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciaturma71/Farmaciaturma71ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaturma71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farmaciaturma71ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
